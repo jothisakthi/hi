b39c0fd1e54e45b11478ee2b1d62cdee4d2289e8
jothisakthi <jothisakthivel23@gmail.com>
2016-07-20 10:38:42 +0530

    x
    x

---
+++ pos.java
@@ -0,0 +1,16 @@
+public class jo
+{
+public static void main(String args[])
+{
+int a;
+a=Integer.parseInt(args[0]);
+if(a>0)
+{
+System.out.println("positive");
+}
+else if(a<0)
+{
+System.out.println("negative");
+}
+}
+}
