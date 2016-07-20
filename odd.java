fe95cfbbbf2728f06425b31f13bfec1767729e5c
jothisakthi <jothisakthivel23@gmail.com>
2016-07-20 10:42:36 +0530

    ww
    e

---
+++ odd.java
@@ -0,0 +1,16 @@
+public class jo
+{
+public static void main(String args[])
+{
+int a;
+a=Integer.parseInt(args[0]);
+if(a%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
